4cebdd9070080f236e12aad35998b8f1125f3191
Georgy <marakhovskij@gmail.com>
2021-05-10 10:38:32 +0300

    Исправил верстку

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const PostListItem = () => {
   return (
-    <li className='app-list-item d-flex justify-comtent-between'>
+    <li className='app-list-item d-flex justify-content-between'>
       <span className='app-list-item-label lead'>
         Message text, text, text...
       </span>
-      <div className='d-flex justify-comtent-center align-items-center'>
+      <div className='d-flex justify-content-center align-items-center'>
         <button type='button' className='btn-star btn-sm'>
           <i className='bi bi-star'/>
         </button>
