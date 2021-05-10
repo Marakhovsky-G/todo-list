b7235049727737188e929807725d5574235b3832
Georgy <marakhovskij@gmail.com>
2021-05-10 20:29:42 +0300

    Фильтр постов.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ export default class App extends Component {
         { label: 'Thats so good', important: false, like: false, id: 2 },
         { label: 'I need a break...', important: true, like: false, id: 3 }
       ],
-      term: ''
+      term: '',
+      filter: 'all'
     };
     this.maxId = 4;
   }
@@ -91,16 +92,28 @@ export default class App extends Component {
     });
   }
 
+  filterPost(items, filter) {
+    if (filter === 'like') {
+      return items.filter(item => item.like)
+    } else {
+      return items
+    }
+  }
+
   onUpdateSearch = (term) => {
     this.setState({term})
   }
 
+  onFilterSelect = (filter) => {
+    this.setState({filter})
+  }
+
   render() {
-    const {data, term} = this.state,
+    const {data, term, filter} = this.state,
       liked = data.filter(item => item.like).length,
       allPosts = data.length;
 
-    const visiblePosts = this.searchPost(data, term);
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
     return (
       <div className='app'>
@@ -112,7 +125,10 @@ export default class App extends Component {
           <SearchPanel
             onUpdateSearch={this.onUpdateSearch}
           />
-          <PostStatusFilter />
+          <PostStatusFilter
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
         <PostList
           posts={visiblePosts}
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Button } from 'reactstrap';
 import './post-status-filter.css';
 
-const PostStatusFilter = () => {
-  return (
-    <div className='btn-group'>
-      <Button outline color='primary'>Все</Button>
-      <Button outline color='primary'>Избранные</Button>
-    </div>
-  )
-}
+export default class PostStatusFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.buttons = [
+      {name: 'all', label: 'Все'},
+      {name: 'like', label: 'Избранные'},
+    ]
+  }
 
-export default PostStatusFilter;
+  render() {
+    const buttons = this.buttons.map(({name, label}) => {
+      const {filter, onFilterSelect} = this.props;
+      const active = filter === name;
+      const clazz = active ? 'btn-primary' : 'btn-outline-primary'
+      return (
+        <button
+          key={name}
+          type='button'
+          className={`btn ${clazz}`}
+          onClick={() => onFilterSelect(name)}
+          >{label}
+        </button>
+      )
+    })
+    return (
+      <div className='btn-group'>
+        {buttons}
+      </div>
+    )
+  }
+}
