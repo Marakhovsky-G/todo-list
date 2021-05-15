6d38d7c7cea5f8c48d2af0fc9156ef3c3189b040
Georgy <marakhovskij@gmail.com>
2021-05-15 14:57:22 +0300

    Логика окончаний и проценты выполнения

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './app-header.css';
 
-const AppHeader = ({liked, allPosts}) => {
+const AppHeader = ({done, allPosts}) => {
+  let postfix = '';
+
+  if (allPosts === 0) {
+    postfix = 'дел';
+  } else if (allPosts === 1) {
+    postfix = 'дело';
+  } else if (allPosts > 1 && allPosts < 5) {
+    postfix = 'дела';
+  } else {
+    postfix = 'дел';
+  }
+
   return (
     <div className="app-header d-flex align-items-center">
-      <h1 className='text-primary'>Georgy</h1>
-      <small className='text-muted'>{allPosts} записей, из них понравилось {liked}</small>
+      <h1 className='text-primary'>To do list</h1>
+      <small className='text-muted'>{allPosts} {postfix}, из них выполненно {done}  ({allPosts === 0 ? '0' : Math.round(100 / allPosts * done)}%)</small>
     </div>
   )
 }
